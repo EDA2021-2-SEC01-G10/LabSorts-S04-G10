409dc5857e2272fefc23085a5b3331da76e990e6
Giovanny <e.parra@uniandes.edu.co>
2021-09-14 20:56:23 -1200

    Entrega Ejemplo – laboratorio 4

--- App/view.py
+++ App/view.py
@@ -86,7 +86,7 @@ def printBestBooks(books):
 def printSortResults(ord_books, sample=10):
     size=lt.size(ord_books)
     if size > sample: 
-       print("Los primeros ",sample," libros ordenados son:" 
+       print("Los primeros ",sample," libros ordenados son:" )
        i=1 
        while i <= sample:
              book=lt.lt.getElement(ord_books,i)
